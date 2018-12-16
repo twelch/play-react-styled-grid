cd94870a349cc05f3a1a1da064f7bf9747d1e4e0
Tim Welch <tim.j.welch@gmail.com>
2018-12-15 21:40:10 -0800

    Initial add with storybook, react-router, styled-components, vscode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 40vmin;
+`
+const AppHeader = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+const AppIntro = styled.p`
+  font-size: large;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+      <AppWrapper>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <AppIntro>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </AppIntro>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,8 +49,8 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
-      </div>
+        </AppHeader>
+      </AppWrapper>
     );
   }
 }
