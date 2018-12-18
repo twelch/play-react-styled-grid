f6ac00ce4e497e98582c4d3dda4551e52af35319
Tim Welch <tim.j.welch@gmail.com>
2018-12-18 13:14:49 -0800

    Add dashboard main page, fix style insertion order

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Normalize } from 'styled-normalize';
 import { BaseStyle } from './BaseStyle';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -12,19 +12,35 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import PinDropIcon from '@material-ui/icons/PinDrop';
 import BarChartIcon from '@material-ui/icons/BarChart';
+import DashIcon from '@material-ui/icons/Dashboard';
+
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+
+import yellow from '@material-ui/core/colors/yellow';
 
 import mapboxgl from 'mapbox-gl';
 import logo from './logo.svg';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+// Start with MUI dark theme as base
 const muiTheme = createMuiTheme({
   palette: {
-    type: 'dark' // Switching the dark mode on is a single property value change.
+    type: 'dark'
   },
   typography: { useNextVariants: true }
 });
 
-const headerHeight = '2.5rem';
+// Change JSS injection order so that styled-components wins, and can be used to override MUI components
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  // Custom insertion point, see index.html head placement
+  insertionPoint: document.getElementById('jss-insertion-point')
+});
+
+const headerHeight = '48px';
 const sidebarWidth = '180px';
 
 const OneColLayout = styled.div`
@@ -74,16 +90,20 @@ const SearchBar = styled.div`
 `;
 
 const Profile = styled.div`
-  width: 200px;
+  min-width: 160px;
   display: flex;
   align-items: center;
+  justify-content: center;
   background-color: #ddd;
+  padding: 10px 5px;
 `;
 
-const ProfileBar = styled.div`
-  flex-grow: 1;
-  padding: 3px;
-  background-color: #bbb;
+const ProfileName = styled.div`
+  padding-right: 20px;
+`;
+
+const ProfileAvatar = styled(Avatar)`
+  background-color: ${yellow[500]};
 `;
 
 const Content = styled.div`
@@ -105,15 +125,24 @@ const MainContent = styled.div`
   height: calc(100vh - ${headerHeight});
 `;
 
+const StyledDash = styled.div`
+  height: 100%;
+  flex-grow 1;
+`;
+
+const StyledReport = styled.div`
+  height: 100%;
+  flex-grow 1;
+`;
+
 const StyledMap = styled.div`
   height: 100%;
   flex-grow: 1;
 `;
 
-const StyledInfo = styled.div`
-  height: 100%;
-  flex-grow 1;
-`;
+const DashView = props => {
+  return <StyledDash>Dashboard</StyledDash>;
+};
 
 class MapView extends Component {
   componentDidMount() {
@@ -133,59 +162,75 @@ class MapView extends Component {
   }
 }
 
-const InfoView = props => {
-  return <StyledInfo>Info panel</StyledInfo>;
+const ReportView = props => {
+  return <StyledReport>Report panel</StyledReport>;
 };
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={muiTheme}>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
         <ThemeProvider theme={muiTheme}>
-          <React.Fragment>
-            <Normalize />
-            <BaseStyle />
-            <Router>
-              <OneColLayout>
-                <Header>
-                  <Logo>
-                    <AppLogo src={logo} alt="logo" />
-                    My App
-                  </Logo>
-                  <Search>
-                    <SearchBar>Search</SearchBar>
-                  </Search>
-                  <Profile>
-                    <ProfileBar>Profile</ProfileBar>
-                  </Profile>
-                </Header>
-                <Content>
-                  <NavigationMenu>
-                    <List>
-                      <ListItem button key={'map'} component={Link} to="/map">
-                        <ListItemIcon>
-                          <PinDropIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={'Map View'} />
-                      </ListItem>
-                      <ListItem button key={'info'} component={Link} to="/info">
-                        <ListItemIcon>
-                          <BarChartIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={'Reports'} />
-                      </ListItem>
-                    </List>
-                  </NavigationMenu>
-                  <MainContent>
-                    <Route exact path="/map" component={MapView} />
-                    <Route exact path="/info" component={InfoView} />
-                  </MainContent>
-                </Content>
-              </OneColLayout>
-            </Router>
-          </React.Fragment>
+          {/* Pass muiTheme to styled-components for access */}
+          <MuiThemeProvider theme={muiTheme}>
+            <React.Fragment>
+              <Normalize />
+              <BaseStyle />
+              <Router>
+                <OneColLayout>
+                  <Header>
+                    <Logo>
+                      <AppLogo src={logo} alt="logo" />
+                      My App
+                    </Logo>
+                    <Search>
+                      <SearchBar>Search</SearchBar>
+                    </Search>
+                    <Profile>
+                      <ProfileName>Bob B.</ProfileName>
+                      <ProfileAvatar alt="Bob B.">BB</ProfileAvatar>
+                    </Profile>
+                  </Header>
+                  <Content>
+                    <NavigationMenu>
+                      <List>
+                        <ListItem button key={'dash'} component={Link} to="/">
+                          <ListItemIcon>
+                            <DashIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={'Dashboard'} />
+                        </ListItem>
+                        <ListItem button key={'map'} component={Link} to="/map">
+                          <ListItemIcon>
+                            <PinDropIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={'Map View'} />
+                        </ListItem>
+                        <ListItem
+                          button
+                          key={'report'}
+                          component={Link}
+                          to="/report"
+                        >
+                          <ListItemIcon>
+                            <BarChartIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={'Reports'} />
+                        </ListItem>
+                      </List>
+                    </NavigationMenu>
+                    <MainContent>
+                      <Route exact path="/" component={DashView} />
+                      <Route exact path="/map" component={MapView} />
+                      <Route exact path="/report" component={ReportView} />
+                    </MainContent>
+                  </Content>
+                </OneColLayout>
+              </Router>
+            </React.Fragment>
+          </MuiThemeProvider>
         </ThemeProvider>
-      </MuiThemeProvider>
+      </JssProvider>
     );
   }
 }
